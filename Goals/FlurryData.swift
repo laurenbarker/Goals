5a7f0e57ae6999067f03bc7a929960c2b01f6d49
Lauren Barker <leb2dg@virginia.edu>
2015-11-23 17:16:32 -0500

    allow api calls in info.plist

--- Goals/FlurryData.swift
+++ Goals/FlurryData.swift
@@ -1,36 +1,29 @@
 //
-//  AnalyticsViewController.swift
+//  FlurryData.swift
 //  Goals
 //
 //  Created by Lauren Barker on 11/23/15.
 //  Copyright © 2015 Lauren Barker. All rights reserved.
 //
 
-import UIKit
 import Foundation
 
-class AnalyticsViewController: UIViewController {
+class FlurryData: NSObject {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        loadData()
-        
-        // Do any additional setup after loading the view.
-    }
+    // API Access code = PRQGR6S2NZK5WGR8YDT5
+    // API Key = 6FJJ9XQKW77JTSHP97NG
     
     func loadData() {
-        
+
         let task = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: "http://api.flurry.com/eventMetrics/Summary?apiAccessCode=PRQGR6S2NZK5WGR8YDT5&apiKey=6FJJ9XQKW77JTSHP97NG&startDate=2015-11-23&endDate=2015-11-27")!) {(data, response, error) in
             print(NSString(data: data!, encoding: NSUTF8StringEncoding))
         }
-        
+    
         task.resume()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
+    
+    
 }
